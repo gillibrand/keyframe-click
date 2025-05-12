6e834e95dd5f39081f34515ec6466ac1426b4577
gillibrand <gillibrand@users.noreply.github.com>
2025-05-12 00:58:16 +0000

    deploy: 3375c4a07b8b62647144117d49183fc940ec7cf2

--- assets/AboutPage-Cp78Eba1.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-DxMDfp_F.js";function o(){return t.jsx("h2",{children:"About Page"})}export{o as default};
---
+++ assets/AboutPage-DQFIIdEh.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DNy4b6cI.js";function o(){return t.jsx("h2",{children:"About Page"})}export{o as default};
