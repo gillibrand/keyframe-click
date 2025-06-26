4ecc812397d0baeae46c6f69aa8a6954cc2de815
Jay Gillibrand <jay.gillibrand@gmail.com>
2025-06-25 20:13:24 -0700

    Banner for Tailwind

--- src/app/Banner.tsx
+++ src/app/Banner.tsx
@@ -1,78 +1,21 @@
 import Github from "@images/github-mark.svg?react";
-import { useRouter } from "@router/useRouter";
-import { isDevMode, stopEvent } from "@util";
-import "./Banner.css";
+import { isDevMode } from "@util";
+import { BannerLink } from "./BannerLink";
 
 export function Banner() {
-  const { route, preloadRoute, gotoTimeline } = useRouter();
-
-  /**
-   * @param href The HREF path to return. This should be a #/path to the current router.
-   * @param preload If true, will add mouse and focus event to preload the load.
-   * @returns A const object of the given HREF, plus the aria attributes to mark this as the current
-   *   page if it matches the current route. Spread this into an anchor.
-   */
-  function href(href: string, preload?: boolean) {
-    const normalHref = href.slice(1);
-    const isCurrent = route === normalHref;
-
-    const preloadIfNeeded = !preload
-      ? undefined
-      : () => {
-          preloadRoute(normalHref);
-        };
-
-    return {
-      href,
-      "aria-current": isCurrent ? "page" : undefined,
-      onMouseEnter: preloadIfNeeded,
-      onFocus: preloadIfNeeded,
-    } as const;
-  }
-
-  /**
-   * Goes to the home timeline page. We manually call `setRoute` so that we can make a clean URL
-   * without a hash in it. If we just go to path / then a full page reload would happen. This
-   * manually changes the URL with `pushState` then sets the home route.
-   *
-   * @param e Link click event to cancel.
-   */
-  function handleGotoTimeline(e: React.MouseEvent) {
-    stopEvent(e);
-    gotoTimeline();
-  }
-
   return (
-    <header className="py-4x Banner sm:text-sm">
+    <header className="border-b-2 border-black bg-white sm:text-sm">
       <div className="wrapper flex items-end gap-8">
-        <h1
-          className="Banner__title hidden cursor-default text-2xl font-bold text-nowrap sm:block"
-          onClick={handleGotoTimeline}
-        >
+        <h1 className="Banner__title my-1 hidden cursor-default text-2xl font-bold text-nowrap sm:block">
           Keyframe Click
         </h1>
 
-        <nav className="Banner__nav flex gap-4">
-          <a className="Banner__link leading-none" {...href("#/")} onClick={handleGotoTimeline}>
-            Timeline
-          </a>
-          <a className="Banner__link leading-none" {...href("#/demos")}>
-            Demos
-          </a>
-          {/*
-          <a className="Banner__link leading-none" {...href("#/help")}>
-            Help
-          </a> 
-          */}
-          <a className="Banner__link leading-none" {...href("#/about", true)}>
-            About
-          </a>
+        <nav className="flex gap-4">
+          <BannerLink href="#/">Timeline</BannerLink>
+          <BannerLink href="#/demos">Demos</BannerLink>
+          <BannerLink href="#/about">About</BannerLink>
 
-          {isDevMode && (
-            <a className="Banner__link desktop-only leading-none" {...href("#/debug", true)}>
-              dev
-            </a>
-          )}
+          {isDevMode && <BannerLink href="#/debug">dev</BannerLink>}
         </nav>
 
         <div className="text-light desktop-only ml-auto flex items-center gap-4 py-2">
---
+++ src/app/BannerLink.tsx
@@ -0,0 +1,73 @@
+import { useRouter } from "@router/useRouter";
+import { stopEvent } from "@util";
+import clsx from "clsx";
+import { PropsWithChildren } from "react";
+
+interface Props extends PropsWithChildren {
+  href: string;
+  preload?: boolean;
+}
+
+export function BannerLink({ href, preload, children }: Props) {
+  const { route, preloadRoute, gotoTimeline } = useRouter();
+
+  const isCurrentRoute = route === href.slice(1);
+
+  /**
+   * @param href The HREF path to return. This should be a #/path to the current router.
+   * @param preload If true, will add mouse and focus event to preload the load.
+   * @returns A const object of the given HREF, plus the aria attributes to mark this as the current
+   *   page if it matches the current route. Spread this into an anchor.
+   */
+  function makeHref() {
+    const normalHref = href.slice(1);
+    const isCurrent = route === normalHref;
+
+    const preloadIfNeeded = !preload
+      ? undefined
+      : () => {
+          preloadRoute(normalHref);
+        };
+
+    return {
+      href,
+      "aria-current": isCurrent ? "page" : undefined,
+      onMouseEnter: preloadIfNeeded,
+      onFocus: preloadIfNeeded,
+    } as const;
+  }
+
+  /**
+   * Goes to the home timeline page via the router to ensure a clean URL. We manually call
+   * `setRoute` so that we can make a clean URL without a hash in it. If we just go to path / then a
+   * full page reload would happen. This manually changes the URL with `pushState` then sets the
+   * home route.
+   *
+   * @param e Link click event to cancel.
+   */
+  function handleTimelineClick(e: React.MouseEvent) {
+    if (href === "#/") {
+      stopEvent(e);
+      gotoTimeline();
+    }
+  }
+
+  // This is a little slice of orange to cover the bottom border/shadow of the tab and make it look like part of the page.
+  const activeBottomMask =
+    "after:absolute after:-bottom-1 after:left-0 after:h-2 after:w-full after:bg-neo-orange after:content-['']";
+
+  return (
+    <a
+      className={clsx(
+        "relative mt-2 rounded-t-lg border-2 border-b-0 px-4 py-2 leading-none font-bold",
+        isCurrentRoute ? "bg-neo-orange shadow-hard border-black" : "border-black bg-[#fbd677]",
+        isCurrentRoute && activeBottomMask
+      )}
+      {...makeHref()}
+      onClick={handleTimelineClick}
+      aria-current={isCurrentRoute}
+    >
+      {children}
+    </a>
+  );
+}
