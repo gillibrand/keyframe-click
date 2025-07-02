8ec5bc11aaf6ae3e78d50daa9dc8d07c654f3f9e
Jay Gillibrand <jay.gillibrand@gmail.com>
2025-07-02 15:42:52 -0700

    Default to text-sm on all pages

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -3,7 +3,7 @@ import CreatedBy from "./CreatedBy.mdx";
 
 export function AboutPage() {
   return (
-    <main className="flex grow flex-col text-base">
+    <main className="flex grow flex-col">
       <div className="wrapper j-prose flex grow flex-col">
         <div className="tile stack flex grow flex-col">
           <div className="grow space-y-4">
